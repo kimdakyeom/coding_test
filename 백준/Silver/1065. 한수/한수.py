3ebac931712fd1011dc4de2bcc424b6f2c000792
kimdakyeom <74820869+kimdakyeom@users.noreply.github.com>
2022-08-05 17:31:26 +0900

    [Silver IV] Title: 한수, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1065. 한수/한수.py
@@ -0,0 +1,10 @@
+n = int(input())
+hansu = 0
+
+for i in range(1, n+1):
+    l = list(map(int, str(i)))
+    if i < 100:
+        hansu += 1
+    elif l[0] - l[1] == l[1] - l[2]:
+        hansu += 1
+print(hansu)
