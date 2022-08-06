e26a0a22eb5626396a57b4db20da35e2bdfe9382
kimdakyeom <74820869+kimdakyeom@users.noreply.github.com>
2022-08-06 12:57:23 +0900

    0806

--- SWEA/D3/swea_1216.py
+++ SWEA/D3/swea_1216.py
@@ -1,49 +1,34 @@
 import sys
-sys.stdin = open("swea_1216_input.txt", 'rt', encoding='UTF8')
-
-def solve(y, x, wh):
-    global answer
-    result = ''
-    length = 8
-
-    while True:
-        if wh == 'w':
-            for i in range(length):
-                result += board[y][x + i]
-                print(result)
-            for j in range(length // 2):
-                if result[j] == result[-1 - j]:
-                    answer = len(result)
-                    break
-        else:
-            for i in range(length):
-                result += board[y + i][x]
-            for j in range(length // 2):
-                if result[j] == result[-1 - j]:
-                    answer = len(result)
-                    break
-        length -= 1
-    return
-
+sys.stdin = open("swea_1216_input.txt")
 
 for t in range(1, 11):
-    # n = int(input())
-    # arr = [input() for _ in range(8)]
+    tc = int(input())
+    board = [list(input()) for _ in range(100)]
 
-    board = [['C', 'B', 'C', 'A', 'B', 'B', 'A', 'C'],
-            ['B', 'B', 'A', 'B', 'C', 'A', 'B', 'A'],
-            ['A', 'B', 'C', 'B', 'C', 'C', 'C', 'A'],
-            ['B', 'A', 'C', 'C', 'A', 'A', 'B', 'B'],
-            ['B', 'C', 'B', 'C', 'A', 'C', 'B', 'C'],
-            ['C', 'A', 'B', 'A', 'C', 'A', 'C', 'B'],
-            ['C', 'A', 'A', 'A', 'C', 'C', 'A', 'B'],
-            ['C', 'B', 'A', 'B', 'A', 'C', 'A', 'C']]
+    def check(string, length):
+        max_ = length
+        while length <= 100:
+            ans = 0
+            for i in range(100):
+                for j in range(101-length):
+                    word = string[i][j:j+length]
+                    if word == word[::-1]:
+                        max_ = length
+                        ans = 1
+                        break
+                if ans == 1:
+                    break
+            length += 1
+        return max_
 
-    for i in range(0, 8):
-        for j in range(9 - n):
-            solve(i, j, 'w')
-    for i in range(9 - n):
-        for j in range(8):
-            solve(i, j, 'h')
-    
-    print(f'#{t} {answer}')
+    row = check(board, 1)
+
+    trans_board = [[0] * 100 for _ in range(100)]
+
+    for i in range(100):
+        for j in range(100):
+            trans_board[i][j] = board[j][i]
+
+    col = check(trans_board, 1)
+
+    print(f'#{t} {max(row, col)}')
