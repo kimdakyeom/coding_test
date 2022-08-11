0bac1bb5c154197003291de6d5602904aed200a3
kimdakyeom <74820869+kimdakyeom@users.noreply.github.com>
2022-08-11 15:06:04 +0900

    [Bronze II] Title: 연속구간, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2495. 연속구간/연속구간.py
@@ -0,0 +1,12 @@
+for i in range(3):
+    s = list(input())
+    
+    sum_ = 0
+    sum_list = []
+    for j in range(len(s) - 1):
+        if s[j] == s[j+1]:
+            sum_ += 1
+        else:
+            sum_ = 0
+        sum_list.append(sum_)
+    print(max(sum_list) + 1)
