12ead8354a796d4cd7d342db0864f85f6418ca1b
kimdakyeom <74820869+kimdakyeom@users.noreply.github.com>
2022-08-11 13:36:01 +0900

    [Bronze V] Title: 영수증, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,12 @@
+x = int(input())
+n = int(input())
+
+sum_ = 0
+for i in range(n):
+    a, b = map(int, input().split())
+    sum_ += a *b
+
+if sum_ == x:
+    print('Yes')
+else:
+    print('No')
