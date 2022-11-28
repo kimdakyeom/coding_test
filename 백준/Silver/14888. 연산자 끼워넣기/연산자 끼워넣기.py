a0ccae7048e8d817e29ff87085f690fcf07cbca3
kimdakyeom <74820869+kimdakyeom@users.noreply.github.com>
2022-11-28 22:14:26 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 92 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,40 +1,28 @@
 import sys
-# sys.stdin = open("input2.txt", "r")
-
 from itertools import permutations
 input = sys.stdin.readline
 n = int(input())
 num_list = list(map(int, input().split()))
 operator_input = list(map(int, input().split()))
-operator_list = ["+", "-", "*", "/"]
-operator = []
-
-for idx, op in enumerate(operator_input):
-    for k in range(op):
-        operator.append(operator_list[idx])
 
 maximum = -1e8
 minimum = 1e8
+def dfs(depth, total, plus, minus, multiply, divide):
+    global maximum, minimum
+    if depth == n:
+        maximum = max(total, maximum)
+        minimum = min(total, minimum)
+        return
 
-for op in permutations(operator, n-1):
-    answer = num_list[0]
-    for i in range(n-1):
-        if op[i] == "+":
-            answer += num_list[i+1]
-        elif op[i] == "-":
-            answer -= num_list[i+1]
-        elif op[i] == "*":
-            answer *= num_list[i+1]
-        elif op[i] == "/":
-            if answer > 0:
-                answer //= num_list[i+1]
-            else:
-                answer = -answer
-                answer //= num_list[i+1]
-                answer = -answer
-    if answer > maximum:
-        maximum = answer
-    if answer < minimum:
-        minimum = answer
+    if plus:
+        dfs(depth + 1, total + num_list[depth], plus - 1,  minus, multiply, divide)
+    if minus:
+        dfs(depth + 1, total - num_list[depth], plus,  minus - 1, multiply, divide)
+    if multiply:
+        dfs(depth + 1, total * num_list[depth], plus,  minus, multiply - 1, divide)
+    if divide:
+        dfs(depth + 1, int(total / num_list[depth]), plus,  minus, multiply, divide - 1)
+        
+dfs(1, num_list[0], operator_input[0], operator_input[1], operator_input[2], operator_input[3],)
 print(maximum)
 print(minimum)
