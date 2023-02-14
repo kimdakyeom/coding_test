7b354b3cd774309143bc0a5cd6f4efffef56cc5f
kimdakyeom <74820869+kimdakyeom@users.noreply.github.com>
2023-02-14 16:28:55 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 232 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -4,11 +4,18 @@ import sys
 string = input().rstrip()
 
 n = int(input())
-for _ in range(n):
+arr = [[0 for i in range(26)] for i in range(len(string))]
+arr[0][ord(string[0])-97] = 1
+
+for i in range(1, len(string)):
+    arr[i][ord(string[i])-97] = 1
+    for j in range(26):
+        arr[i][j] += arr[i-1][j]
+for i in range(n):
     a, s, e = input().split()
     s, e = int(s), int(e)
-    answer = 0
-    for i in range(s, e+1):
-        if str(string[i]) == a:
-            answer += 1
+    if s > 0:
+        answer = arr[e][ord(a)-97] - arr[s-1][ord(a)-97]
+    else:
+        answer = arr[e][ord(a)-97]
     print(answer)
