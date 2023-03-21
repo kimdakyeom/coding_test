b6627623435161ea348c8f3a7c4276d11d96b660
kimdakyeom <74820869+kimdakyeom@users.noreply.github.com>
2023-03-21 10:20:47 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10744 ms, Memory: 215476 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
@@ -1,20 +1,8 @@
 import sys
+
+from pprint import pprint
 input = sys.stdin.readline
 from collections import deque
-
-def bfs(com):
-    cnt = 0
-    queue = deque([com])
-    visited = [0 for _ in range(n+1)]
-    visited[com] = 1
-    while queue:
-        tmp = queue.popleft()
-        cnt += 1
-        for i in graph[tmp]:
-            if not visited[i]:
-                visited[i] = 1
-                queue.append(i)
-    return cnt
 
 n, m = map(int, input().split())
 graph = [[] for _ in range(n+1)]
@@ -22,10 +10,32 @@ graph = [[] for _ in range(n+1)]
 for i in range(m):
     a, b = map(int, input().split())
     graph[b].append(a)
+
+def bfs(cur):
+    global tmp_max
+    q = deque([cur])
+    visited[cur] = 1
+    cnt = 0
+    while q:
+        c = q.popleft()
+        cnt += 1
+        for n in graph[c]:
+            if not visited[n]:
+                q.append(n)
+                visited[n] = 1
+    return cnt
+
+answer = 0
+answer_list = []
 max_ = 0
-answer = [0 for _ in range(n+1)]
 for i in range(1, n+1):
-    answer[i] = bfs(i)
-for i in range(1, n+1):
-    if answer[i] == max(answer):
-        print(i, end=' ')
+    visited = [0] * (n+1)
+    answer_list.append((i, bfs(i)))
+answer_list.sort(key=lambda x : (-x[1], x[0]))
+max_ = answer_list[0][1]
+
+result = []
+for a in answer_list:
+    if a[1] == max_:
+        result.append(a[0])
+print(*result)
